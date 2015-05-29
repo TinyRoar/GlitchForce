e960a9cd8acea53e12ae74eb815f64ac07bd4079
unknown <lassekluever@web.de>
2015-05-29 23:36:30 +0200

    Added GlitchMocks Added SceneFinish GameObject Added GlitchCollector Modified PlayerPrefab Modified GlitchPrefab

---
+++ Assets/Scripts/Gameplay/GlitchCollector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlitchCollector : MonoBehaviour {
+
+    private GlitchMock lastSelectedGlitch;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Glitch")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void CollectGlitch(GlitchMock mock)
+    {
+
+    }
+}
