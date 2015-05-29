ca1d9ed625dc5815810e4b754e205b9868c0f5ef
unknown <lassekluever@web.de>
2015-05-29 23:01:04 +0200

    Hier ist dein Singleton Da Rio!!!

---
+++ Assets/Scripts/Engine/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour, new() {
+
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+	// Use this for initialization
+	void Awake () {
+        instance = this as T;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
