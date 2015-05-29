69387ceea9d6f33c2076514f2ccfca068866dbfa
Moritz Binder <moritz_binder@gmx.de>
2015-05-30 00:07:51 +0200

    added: movement script

--- Assets/Scripts/Gameplay/Movement.cs
+++ Assets/Scripts/Gameplay/Movement.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+    public float speed = 3.0f;
+    public float jumpHeight = 50.0f;
+    private Player player;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        player = this.GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+
+
+	    if(Input.GetKey(KeyCode.A) && player.ThisPlayer == Player.PlayerID.Player1)
+        {
+            transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D) && player.ThisPlayer == Player.PlayerID.Player1)
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && player.ThisPlayer == Player.PlayerID.Player1)
+        {
+            transform.position += new Vector3(0, jumpHeight, 0) * Time.deltaTime;
+        }
+
+        
 	}
 }
