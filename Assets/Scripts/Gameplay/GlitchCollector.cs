7de00f8459ccfd305103911766312812a94be2e7
unknown <lassekluever@web.de>
2015-05-30 00:30:09 +0200

    Modified Scene

--- Assets/Scripts/Gameplay/GlitchCollector.cs
+++ Assets/Scripts/Gameplay/GlitchCollector.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 public class GlitchCollector : MonoBehaviour {
-
-    private GlitchMock lastSelectedGlitch;
+    
+    private GlitchMock selectedGlitch;
+    private GameObject selectedGlitchGameObject;
+    private string lastSelectedGlitchGameObject = "";
 
 	// Use this for initialization
 	void Start () {
@@ -15,37 +17,49 @@ public class GlitchCollector : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag == "Glitch" && lastSelectedGlitchGameObject == "")
+        {
+            Debug.Log(lastSelectedGlitchGameObject);
+            if (selectedGlitch != null)
+            {
+                if (selectedGlitch is PlayerGlitchMock)
+                {
+                    DropPlayerGlitch(other.transform.position);
+                    lastSelectedGlitchGameObject = other.gameObject.name;
+                }
+                else
+                {
+                    DropGlobalGlitch();
+                }
+            }
+            CollectGlitch(other.GetComponent<GlitchMock>());
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
         if (other.tag == "Glitch")
         {
-            Destroy(other.gameObject);
+            Debug.Log("Exit");
+            lastSelectedGlitchGameObject = "";
         }
     }
 
     private void CollectGlitch(GlitchMock mock)
     {
-
+        selectedGlitch = mock;
+        mock.gameObject.SetActive(false);
+        selectedGlitchGameObject = mock.gameObject;
     }
 
-    private void DropLastGlitch()
+    private void DropPlayerGlitch(Vector3 position)
     {
-        if(lastSelectedGlitch is PlayerGlitchMock)
-        {
-
-        }
-
-        else
-        {
-
-        }
-    }
-
-    private void DropPlayerGlitch()
-    {
-
+        selectedGlitchGameObject.SetActive(true);
+        selectedGlitchGameObject.transform.position = position;
     }
 
     private void DropGlobalGlitch()
     {
-
+        selectedGlitchGameObject.SetActive(true);
     }
 }
