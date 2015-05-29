69f751e8c1893cc1dec768cc7f101baeb6afb4ce
DarioDomiDE <dario-domi@hotmail.de>
2015-05-30 00:09:38 +0200

    Added SceneFinish to Player

--- Assets/Scripts/Scenes/SceneFinish.cs
+++ Assets/Scripts/Scenes/SceneFinish.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class SceneFinish : MonoBehaviour
 {
 
-    private int playerInFinisher = 0;
-    private int playerCount = 2;
+    public static int playerInFinisher = 0;
+    public static int playerCount = 2;
 
 	// Use this for initialization
 	void Start () {
@@ -15,25 +15,25 @@ public class SceneFinish : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "SceneFinish")
         {
-            this.playerInFinisher++;
+            SceneFinish.playerInFinisher++;
             Debug.Log("Player go in Finish");
         }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "SceneFinish")
         {
-            this.playerInFinisher--;
+            SceneFinish.playerInFinisher--;
             Debug.Log("Player leave in Finish");
         }
     }
 
     private void CheckFinish()
     {
-        if (this.playerInFinisher >= this.playerCount)
+        if (SceneFinish.playerInFinisher >= SceneFinish.playerCount)
         {
             Config.Instance.CurrentSceneID++;
             Application.LoadLevel(Config.Instance.CurrentSceneID);
