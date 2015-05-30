ca938b327f92562345dd1edc04b21b647b5e0f65
DarioDomiDE <dario-domi@hotmail.de>
2015-05-30 02:13:16 +0200

    Added Jumping final and changed Physics 2D Player can walk in each other

--- Assets/Scripts/Gameplay/Jump.cs
+++ Assets/Scripts/Gameplay/Jump.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class Jump : MonoBehaviour
 {
 
-    private float jumpHeight = 0.5f;
-    private float jumpWidth = 0.5f;
+    private float jumpHeight = 10.0f;
+    private float jumpWidth = 5.0f;
     private Player player;
 
     public enum State { standing, running, jumping }
-    public State currentState = State.standing;
+    private State currentState = State.standing;
 
     private float defaultTime = 1.0f;
     private float jumpTime;
@@ -26,6 +26,11 @@ public class Jump : MonoBehaviour
     {
         DoJump();
 
+        if (Input.GetKeyDown(KeyCode.Space) && currentState != State.jumping && player.ThisPlayer == Player.PlayerID.Player1)
+        {
+            StartJump();
+        }
+
     }
 
     private void StartJump()
@@ -35,7 +40,7 @@ public class Jump : MonoBehaviour
         this.GetComponent<Rigidbody2D>().gravityScale = 0;
     }
 
-    private void EndJump()
+    private void StopJump()
     {
         currentState = State.standing;
         this.GetComponent<Rigidbody2D>().gravityScale = 1;
@@ -50,15 +55,15 @@ public class Jump : MonoBehaviour
         jumpTime -= Time.deltaTime;
         Vector3 pos = this.transform.position;
 
-        pos.y = jumpHeight * jumpTime;
-        Debug.Log(jumpHeight * jumpTime);
-
-
-        // stop
-
-
-        //  pos.x = jumpWidth * Time.deltaTime;
+        pos.y += jumpHeight / 100.0f * jumpTime;
+        pos.x += jumpWidth / 10.0f * Time.deltaTime;
         this.transform.position = pos;
 
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StopJump();
+    }
+
 }
