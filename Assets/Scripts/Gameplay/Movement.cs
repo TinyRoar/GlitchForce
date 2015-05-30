6e09681522f714219cdc1acedc455a14a0bb4593
Awesomaurice <maurice@superawesome.de>
2015-05-30 16:50:05 +0200

    Fixed first two levels

--- Assets/Scripts/Gameplay/Movement.cs
+++ Assets/Scripts/Gameplay/Movement.cs
@@ -44,7 +44,7 @@ public class Movement : MonoBehaviour
             transform.position -= new Vector3(speed*(speedGlitchActive ? glitchSpeed : 1), 0, 0)*Time.deltaTime;
             player.CurrentDirection = Config.Direction.Left;
             Camera.main.GetComponent<CameraZoomer>().ZoomCam(this);
-            this.player.Hero.Play("Run");
+            //this.player.Hero.Play("Run");
             IsMoving = true;
         }
         else if (
@@ -63,13 +63,13 @@ public class Movement : MonoBehaviour
             transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
             player.CurrentDirection = Config.Direction.Right;
             Camera.main.GetComponent<CameraZoomer>().ZoomCam(this);
-            this.player.Hero.Play("Run");
+            //this.player.Hero.Play("Run");
             IsMoving = true;
         }
         // Moving was last frame, but was stopped this Frame
         else if (IsMoving == true)
         {
-            this.player.Hero.Play("Idle");
+            //this.player.Hero.Play("Idle");
             IsMoving = false;
         }
     }
